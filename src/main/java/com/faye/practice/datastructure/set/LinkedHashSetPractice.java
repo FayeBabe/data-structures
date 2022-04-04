f43f5334d5505a3c2a621e450e684d1ecb596165
Faye Mathijssen <faye.mathijssen@mykolab.com>
2022-04-04 21:48:25 +0200

    add LinkedHashSetPractice stub

--- src/main/java/com/faye/practice/datastructure/set/LinkedHashSetPractice.java
+++ src/main/java/com/faye/practice/datastructure/set/LinkedHashSetPractice.java
@@ -1,4 +1,13 @@
 package com.faye.practice.datastructure.set;
 
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 public class LinkedHashSetPractice {
+
+    Set<String> linkedHashSet = new LinkedHashSet<>();
+
+    {
+        // TODO: explore differences between TreeSet and LinkedHashSet.
+    }
 }
