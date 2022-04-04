f54799b6a985e426eaca708406d76ffa5478d270
Faye Mathijssen <faye.mathijssen@mykolab.com>
2022-04-04 21:45:51 +0200

    add TreeSet practice code

--- src/main/java/com/faye/practice/Main.java
+++ src/main/java/com/faye/practice/Main.java
@@ -2,11 +2,13 @@ package com.faye.practice;
 
 import com.faye.practice.datastructure.list.ArrayListPractice;
 import com.faye.practice.datastructure.set.HashSetPractice;
+import com.faye.practice.datastructure.set.TreeSetPractice;
 
 public class Main {
 
     public static void main(String[] args) {
 //        ArrayListPractice alp = new ArrayListPractice();
         HashSetPractice hsp = new HashSetPractice();
+        TreeSetPractice tsp = new TreeSetPractice();
     }
 }
--- src/main/java/com/faye/practice/datastructure/set/TreeSetPractice.java
+++ src/main/java/com/faye/practice/datastructure/set/TreeSetPractice.java
@@ -1,6 +1,10 @@
 package com.faye.practice.datastructure.set;
 
-import java.util.Set;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.TreeSet;
 
 /*
@@ -11,9 +15,54 @@ import java.util.TreeSet;
  */
 public class TreeSetPractice {
 
-    final Set<String> set = new TreeSet<>();
+    /* Example of a TreeSet of custom objects that can be compared with a Comparator, to make it interesting. */
+    final TreeSet<CustomComponent> set = new TreeSet<>();
 
-   TreeSetPractice() {
+    {
+        /*
+         * TreeSet works mostly like any other Set, but because it implements NavigableSet instead of a plain Set, it has
+         * a notion of the order of its elements. For a given element x, you can search for:
+         * - the greatest element < x with set.lower(x)
+         * - the greatest element <= x with set.floor(x)
+         * - the smallest element >= x with set.ceiling(x)
+         * - the smallest element > x with set.higher(x)
+         * lower() and higher(), respectively, and it can find the first and last elements with floor() and ceiling().
+         */
+        Arrays.asList("1", "2", "3").stream()
+                .forEach(elem -> set.add(new CustomComponent(elem)));
 
-   }
+        CustomComponent lt = set.lower(set.last()); // "2"; using set.last() as a learning aid
+        CustomComponent leq = set.floor(set.last()); // "3"
+        CustomComponent geq = set.ceiling(set.last()); // "3"
+        CustomComponent gt = set.higher(set.last()); // null, because this element doesn't exist
+
+        System.out.println("\nlt: " + lt);
+        System.out.println("leq: " + leq);
+        System.out.println("geq: " + geq);
+        System.out.println("gt: " + gt);
+    }
+
+
+    @RequiredArgsConstructor
+    @Getter
+    static class CustomComponent implements Comparable<CustomComponent> {
+
+        private final String str;
+
+        @Override
+        public int compareTo(CustomComponent o2) {
+            if (str.charAt(0) < o2.str.charAt(0)) {
+                return -1;
+            } else if (str.charAt(0) == o2.str.charAt(0)) {
+                return 0;
+            }
+            return 1;
+        }
+
+        @Override
+        public String toString() {
+            return str;
+        }
+    }
+
 }
